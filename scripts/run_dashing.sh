d47105eb2588632b6a0d0a83af4049138885d8a2
Tim D Kosfeld <tk9066@argo-comp2.princeton.edu>
2023-11-15 21:54:50 -0500

    updated script

--- scripts/run_dashing.sh
+++ scripts/run_dashing.sh
@@ -1,3 +1,3 @@
 mkdir my_sketches
-./dashing2 sketch --outprefix my_sketches -k 8 --cache --cmpout comparison_table COS551_Final/data/*
+./dashing2 sketch --outprefix my_sketches -k 12 --cache --cmpout comparison_table COS551_Final/data/*
 python3 table_to_heatmap.py
